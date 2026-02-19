bed718d586efa5d1ee6dcd8a6f2ec4b37b25fea1
Shamin Ahsan <shamin.ahsan.01@gmail.com>
2026-02-19 03:50:43 -0500

    add method to deal card and method to check if deck is empty

--- deck.cs
+++ deck.cs
@@ -14,6 +14,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 public class Deck
@@ -37,23 +38,47 @@ public class Deck
     //summery
     //use fisher-yates shuffling algoritm for a uniform shuffle
     public void shuffleDeck()
-{
-    Random rand = new Random();
-
-    for (int i = cards.Count - 1; i > 0; i--)
     {
-        int j = rand.Next(0, i + 1);
+        Random rand = new Random();
 
-        // swap cards[i] and cards[j]
-        Card temp = cards[i];
-        cards[i] = cards[j];
-        cards[j] = temp;
+        for (int i = cards.Count - 1; i > 0; i--)
+        {
+            int j = rand.Next(0, i + 1);
+
+            // swap cards[i] and cards[j]
+            Card temp = cards[i];
+            cards[i] = cards[j];
+            cards[j] = temp;
+        }
     }
-}
+
+    //summery
+    //If no cards in deck return true
+    public bool isEmpty()
+    {
+        return remaining == 0;
+    }
 
 
+    //summery
+    //deal the top card of the deck if any cards remaining
+    public Card deal_card()
+    {
+        //check if any cards in deck
+        if (isEmpty()) return null;
+
+        //deal one card
+        Card top = cards[remaining - 1];
+        remaining --;
+        return top;
+    }
+
+
+    //print the suit and value of the card at index
     public string print(int index)
     {
         return cards[index].ToString();
     }
+
+    
 }
