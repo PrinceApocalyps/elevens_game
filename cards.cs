d0db47347a1341cd381001bfb0f6e9c1ddab21b3
Shamin Ahsan <shamin.ahsan.01@gmail.com>
2026-02-19 07:58:55 -0500

    update cards.cs
    Added method GetFaceName and modified method ToString

--- cards.cs
+++ cards.cs
@@ -20,7 +20,6 @@ public class Card
 
     public static string[] suits = { "Clubs", "Hearts", "Diamonds", "Spades" };
     public static int[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
-
     public Card(string suit, int value)
     {
         this.suit = suit;
@@ -33,10 +32,28 @@ public class Card
         return value;
     }
 
+    public string GetFaceName()
+{
+    switch (value)
+    {
+        case 1:
+            return "Ace";
+        case 11:
+            return "Jack";
+        case 12:
+            return "Queen";
+        case 13:
+            return "King";
+        default:
+            return value.ToString();
+    }
+}
+
+
     //summery
     //Returns the value of the called card as string
     public override string ToString()
     {
-        return $"{value} of {suit}";
+        return $"{GetFaceName()} of {suit}";
     }
 }
